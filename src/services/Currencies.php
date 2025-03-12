57da13dfe76fb5ccdb466815e770b78054b151ac
Eric Marthinsen <eric@ericmarthinsen.com>
2025-03-12 02:41:29 -0400

    Update for four decimals

--- src/services/Currencies.php
+++ src/services/Currencies.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link https://craftcms.com/
  * @copyright Copyright (c) Pixel & Tonic, Inc.
@@ -25,107 +26,110 @@ use yii\base\Component;
  */
 class Currencies extends Component
 {
-    private ?ISOCurrencies $_isoCurrencies = null;
+  private ?ISOCurrencies $_isoCurrencies = null;
 
-    public function init()
-    {
-        $this->_isoCurrencies = new ISOCurrencies();
+  public function init()
+  {
+    $this->_isoCurrencies = new ISOCurrencies();
+  }
+
+  /**
+   * @var array
+   */
+  private array $_tellersByIso = [];
+
+  /**
+   * @param \Money\Currency|string $currency
+   * @return Teller
+   */
+  public function getTeller(\Money\Currency|string $currency): Teller
+  {
+    if (is_string($currency)) {
+      $currency = new \Money\Currency($currency);
     }
 
-    /**
-     * @var array
-     */
-    private array $_tellersByIso = [];
+    $parser = new DecimalMoneyParser($this->_isoCurrencies);
+    $formatter = new DecimalMoneyFormatter($this->_isoCurrencies);
+    $roundingMode = Money::ROUND_HALF_UP;
 
-    /**
-     * @param \Money\Currency|string $currency
-     * @return Teller
-     */
-    public function getTeller(\Money\Currency|string $currency): Teller
-    {
-        if (is_string($currency)) {
-            $currency = new \Money\Currency($currency);
-        }
-
-        $parser = new DecimalMoneyParser($this->_isoCurrencies);
-        $formatter = new DecimalMoneyFormatter($this->_isoCurrencies);
-        $roundingMode = Money::ROUND_HALF_UP;
-
-        $iso = $currency->getCode();
-        if (isset($this->_tellersByIso[$iso])) {
-            return $this->_tellersByIso[$iso];
-        }
-
-        $this->_tellersByIso[$iso] = new \Money\Teller(
-            $currency,
-            $parser,
-            $formatter,
-            $roundingMode
-        );
-
-        return $this->_tellersByIso[$iso];
+    $iso = $currency->getCode();
+    if (isset($this->_tellersByIso[$iso])) {
+      return $this->_tellersByIso[$iso];
     }
 
-    /**
-     * Get a currency by it's ISO code.
-     *
-     * @param string $iso
-     * @return \Money\Currency|null
-     */
-    public function getCurrencyByIso(string $iso): ?\Money\Currency
-    {
-        return $this->getAllCurrencies()->first(function(\Money\Currency $currency) use ($iso) {
-            return $currency->getCode() == $iso;
-        });
+    $this->_tellersByIso[$iso] = new \Money\Teller(
+      $currency,
+      $parser,
+      $formatter,
+      $roundingMode
+    );
+
+    return $this->_tellersByIso[$iso];
+  }
+
+  /**
+   * Get a currency by it's ISO code.
+   *
+   * @param string $iso
+   * @return \Money\Currency|null
+   */
+  public function getCurrencyByIso(string $iso): ?\Money\Currency
+  {
+    return $this->getAllCurrencies()->first(function (\Money\Currency $currency) use ($iso) {
+      return $currency->getCode() == $iso;
+    });
+  }
+
+
+  /**
+   * Get a list of all available currencies.
+   *
+   * @return Collection<\Money\Currency>
+   */
+  public function getAllCurrencies(): Collection
+  {
+    return collect($this->_isoCurrencies);
+  }
+
+  /**
+   * @return array
+   */
+  public function getAllCurrenciesList(): array
+  {
+    return $this->getAllCurrencies()->map(function ($currency) {
+      return [
+        'label' => $currency->getCode(), // TODO get name somehow
+        'value' => $currency->getCode(),
+      ];
+    })->toArray();
+  }
+
+  /**
+   * @param Currency|string $currency
+   * @return int
+   */
+  public function getSubunitFor(Currency|string $currency)
+  {
+    // EJM: A little heavy-handed, but should ensure 4 decimals across the board.
+    return 4;
+
+    if (is_string($currency)) {
+      $currency = $this->getCurrencyByIso($currency);
     }
 
+    return $this->_isoCurrencies->subunitFor($currency);
+  }
 
-    /**
-     * Get a list of all available currencies.
-     *
-     * @return Collection<\Money\Currency>
-     */
-    public function getAllCurrencies(): Collection
-    {
-        return collect($this->_isoCurrencies);
+  /**
+   * @param Currency|string $currency
+   * @return int
+   */
+  public function numericCodeFor(Currency|string $currency)
+  {
+    if (is_string($currency)) {
+      $currency = $this->getCurrencyByIso($currency);
     }
 
-    /**
-     * @return array
-     */
-    public function getAllCurrenciesList(): array
-    {
-        return $this->getAllCurrencies()->map(function($currency) {
-            return [
-                'label' => $currency->getCode(), // TODO get name somehow
-                'value' => $currency->getCode(),
-            ];
-        })->toArray();
-    }
-
-    /**
-     * @param Currency|string $currency
-     * @return int
-     */
-    public function getSubunitFor(Currency|string $currency)
-    {
-        if (is_string($currency)) {
-            $currency = $this->getCurrencyByIso($currency);
-        }
-
-        return $this->_isoCurrencies->subunitFor($currency);
-    }
-
-    /**
-     * @param Currency|string $currency
-     * @return int
-     */
-    public function numericCodeFor(Currency|string $currency)
-    {
-        if (is_string($currency)) {
-            $currency = $this->getCurrencyByIso($currency);
-        }
-
-        return $this->_isoCurrencies->subunitFor($currency);
-    }
+    return $this->_isoCurrencies->subunitFor($currency);
+  }
 }
